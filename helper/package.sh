5dd600a88f26074af4b18baad4767b30df86ad07
lunu Bounir <lunu.bounir@gmail.com>
2019-02-02 07:16:17 +0100

    .

--- helper/package.sh
+++ helper/package.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-cd $TRAVIS_BUILD_DIR/helper
+cd helper
 
 # get the official tor executable from www.torproject.org
 cd os/mac
@@ -15,6 +15,7 @@ cd os/win
 wget https://www.torproject.org/dist/torbrowser/8.0.5/tor-win32-0.3.5.7.zip
 7z e tor-win32-0.3.5.7.zip tor.exe *.dll -r
 cd ../..
+pwd
 
 cd os/linux
 mkdir 32
@@ -23,13 +24,15 @@ cd 32
 # tar -xf tor-browser-linux32-8.0.5_en-US.tar.xz "tor-browser_en-US/Browser/TorBrowser/Tor/tor"
 # mv tor-browser_en-US/Browser/TorBrowser/Tor/tor ./tor
 cd ..
+pwd
 
 mkdir 64
 cd 64
-wget https://www.torproject.org/dist/torbrowser/8.0.5/tor-browser-linux64-8.0.5_en-US.tar.xz
-tar -xf tor-browser-linux64-8.0.5_en-US.tar.xz "tor-browser_en-US/Browser/TorBrowser/Tor/tor"
-mv tor-browser_en-US/Browser/TorBrowser/Tor/tor ./tor
+# wget https://www.torproject.org/dist/torbrowser/8.0.5/tor-browser-linux64-8.0.5_en-US.tar.xz
+# tar -xf tor-browser-linux64-8.0.5_en-US.tar.xz "tor-browser_en-US/Browser/TorBrowser/Tor/tor"
+# mv tor-browser_en-US/Browser/TorBrowser/Tor/tor ./tor
 cd ../../..
+pwd
 
 #get the official NodeJS executable
 cd os/mac
@@ -37,6 +40,7 @@ wget https://nodejs.org/dist/v10.15.1/node-v10.15.1-darwin-x64.tar.gz
 tar -xf node-v10.15.1-darwin-x64.tar.gz "node-v10.15.1-darwin-x64/bin/node"
 mv node-v10.15.1-darwin-x64/bin/node ./node
 cd ../..
+pwd
 
 cd os/win
 mkdir 32
@@ -49,12 +53,14 @@ cd 64
 wget https://nodejs.org/dist/v10.15.1/node-v10.15.1-win-x64.zip
 7z e node-v10.15.1-win-x64.zip node.exe -r
 cd ../../..
+pwd
 
 cd os/linux/64
-wget https://nodejs.org/dist/v10.15.1/node-v10.15.1-linux-x64.tar.xz
-tar -xf node-v10.15.1-linux-x64.tar.xz "node-v10.15.1-linux-x64/bin/node"
-mv node-v10.15.1-linux-x64/bin/node ./node
+# wget https://nodejs.org/dist/v10.15.1/node-v10.15.1-linux-x64.tar.xz
+# tar -xf node-v10.15.1-linux-x64.tar.xz "node-v10.15.1-linux-x64/bin/node"
+# mv node-v10.15.1-linux-x64/bin/node ./node
 cd ../../..
+pwd
 
 # mac packaging
 mkdir temp1
@@ -103,6 +109,7 @@ cp ../messaging.js assets/
 
 zip -9 ../win_32.zip * -r
 cd ..
+pwd
 
 #windows 64-bit packaging
 mkdir temp4
@@ -118,6 +125,8 @@ cp ../os/win/tor.exe ./assets/tor/
 cp ../os/win/*.dll ./assets/tor/
 cp ../app.js assets/
 cp ../messaging.js assets/
+pwd
 
 zip -9 ../win_64.zip * -r
 cd ..
+pwd
